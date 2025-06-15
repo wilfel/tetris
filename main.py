a0463d2fc49550aad66620bb2cd216422ca30e83
Will Feldscher <wfeldscher@gmail.com>
2025-06-15 19:29:40 -0400

    Add pygame and create window that closes from pygame tutorial

--- main.py
+++ main.py
@@ -1,6 +1,41 @@
-def main():
-    print("Hello from tetris!")
+import pygame
+from pygame.locals import *
 
+class App:
+    def __init__(self):
+        self._running = True
+        self._display_surf = None
+        self.size = self.weight, self.height = 640, 400
+ 
+    def on_init(self):
+        pygame.init()
+        self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
+        self._running = True
+ 
+    def on_event(self, event):
+        if event.type == pygame.QUIT:
+            self._running = False
+    def on_loop(self):
+        pass
+    def on_render(self):
+        pass
+    def on_cleanup(self):
+        pygame.quit()
+ 
+    def on_execute(self):
+        if not self.on_init():
+            self._running = False
+ 
+        while( self._running ):
+            for event in pygame.event.get():
+                self.on_event(event)
+            self.on_loop()
+            self.on_render()
+        self.on_cleanup()
+    
+def main():
+    theApp = App()
+    theApp.on_execute()
 
 if __name__ == "__main__":
     main()
